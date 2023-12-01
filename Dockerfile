851ba7b6c418afd22cc65ae125e6695227f7b2d9
root <124867543+gpt-partners@users.noreply.github.com>
2023-12-01 07:40:49 +0000

    add voila and nvim

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,18 @@
 FROM python:3.11
 # Set the working directory inside the container
 WORKDIR /root/app
+# Copy dotfiles
+COPY gitconfig /root/.gitconfig
+COPY tmux.conf /root/.tmux.conf
+# Install nvim
+COPY ./install-nvim.sh /root/app
+RUN chmod +x install-nvim.sh
+RUN ./install-nvim.sh
 # Install Jupyter Notebook
 RUN pip install --upgrade pip
 RUN pip install jupyter jupyter_ai jupyterlab-code-formatter black isort voila ipywidgets jupyterlab_vim jupyterlab_sql theme-darcula openai langchain
 # Expose the default Jupyter Notebook port
 EXPOSE 8888
+EXPOSE 8866
 # Start Jupyter Notebook when the container is run
 CMD ["jupyter", "notebook", "--ip=0.0.0.0", "--port=8888", "--no-browser", "--allow-root"]
---
+++ install-nvim.sh
@@ -0,0 +1,20 @@
+# Install dependencies
+apt update
+DEBIAN_FRONTEND=noninteractive apt -y install build-essential ninja-build cmake gettext git ripgrep fd-find unzip tmux zsh locales ca-certificates curl wget gnupg xclip libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libjson-c-dev libwebsockets-dev
+
+# Install neovim
+git clone --depth=1 https://github.com/neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=Release
+make install
+mv /neovim/build/bin/nvim /usr/bin/
+cd .. && rm -r neovim
+
+# Install NvChad
+git clone --depth=1 https://github.com/NvChad/NvChad ~/.config/nvim
+rm -rf $HOME/.config/nvim/.git
+git clone https://github.com/gpt-partners/nvim-config /root/.config/nvim/lua/custom
+nvim --headless +MasonInstallAll +qa
+
+# Fix UTF-8 support
+locale-gen en_IN.UTF-8
